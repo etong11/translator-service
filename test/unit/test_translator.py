c615b09972e5b7adef6fa49eacfa2c0c43893c2b
Luke <lnalewaj@andrew.cmu.edu>
2024-11-13 20:51:27 -0500

    gibberish tests updated

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -13,15 +13,15 @@ def test_llm_gibberish_response():
     is_english, translated_content = translate_content("abcd1234!")
     assert is_english == False
     assert translated_content == "I don't understand your request"
-    is_english, translated_content = translate_content("??¿¿!!??")
-    assert is_english == False
+    is_english2, translated_content = translate_content("??¿¿!!??")
+    assert is_english2 == False
     assert translated_content == "I don't understand your request"
-    is_english, translated_content = translate_content("||| broken sentence.")
-    assert is_english == False
+    is_english3, translated_content = translate_content("||| broken sentence.")
+    assert is_english3 == False
     assert translated_content == "I don't understand your request"
-    is_english, translated_content = translate_content("テスト123 abc xyz")
-    assert is_english == False
+    is_english4, translated_content = translate_content("テスト123 abc xyz")
+    assert is_english4 == False
     assert translated_content == "I don't understand your request"
-    is_english, translated_content = translate_content(".....!!!???...")
-    assert is_english == False
+    is_english5, translated_content = translate_content(".....!!!???...")
+    assert is_english5 == False
     assert translated_content == "I don't understand your request"
